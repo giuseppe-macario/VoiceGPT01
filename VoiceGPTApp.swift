95f7de897fec2a9ff3cf78def55d02af6e136dcb
Giuseppe Macario <141560720+giuseppe-macario@users.noreply.github.com>
2025-06-14 19:21:23 +0200

    Add files via upload

---
+++ VoiceGPTApp.swift
@@ -0,0 +1,17 @@
+//
+//  VoiceGPTApp.swift
+//  VoiceGPT
+//
+//  Created by Giuseppe Macario on 22/04/25.
+//
+
+import SwiftUI
+
+@main
+struct VoiceGPTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
